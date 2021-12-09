4b1b4faa2423d7034af502473ae902943b9a3bb5
AliakseiHryhoryeu <88304337+AliakseiHryhoryeu@users.noreply.github.com>
2021-12-09 15:09:50 +0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path')
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 const isDev = process.env.NODE_ENV === 'development'
@@ -8,67 +8,71 @@ const isProd = !isDev
 
 
 const cssLoaders = extra => {
-    const loaders = [
-      {
-        loader: MiniCssExtractPlugin.loader,
-        options: {
-          hmr: isDev,
-          reloadAll: true
-        },
+  const loaders = [
+    {
+      loader: MiniCssExtractPlugin.loader,
+      options: {
+        hmr: isDev,
+        reloadAll: true
       },
-      'css-loader'
-    ]
-  
-    if (extra) {
-      loaders.push(extra)
-    }
-  
-    return loaders
+    },
+    'css-loader'
+  ]
+
+  if (extra) {
+    loaders.push(extra)
   }
 
-module.exports = {
-    mode: "development", // operating mode webpack
-    entry: "./src/index.js", // entry file
-    output: {
-        path: path.resolve(__dirname, "dist"),  // path to final build folder
-        filename: "[name].[hash].js" // how to name files
-    },
-    plugins: [
-        new HTMLWebpackPlugin({ template: "./src/index.html" }), // create index.html and add in him links to scripts
-        new CleanWebpackPlugin() // cleaning from previous builds
-    ],
-    module: {
-        rules: [ // types of processed files and their loaders
-            {
-                test: /\.css$/,
-                use: cssLoaders()
-              },
-              {
-                test: /\.less$/,
-                use: cssLoaders('less-loader')
-              },
-              {
-                test: /\.s[ac]ss$/,
-                use: cssLoaders('sass-loader')
-              },
-              {
-                test: /\.(png|jpg|svg|gif)$/,
-                use: ['file-loader']
-              },
-              {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                use: ['file-loader']
-              },
-              {
-                test: /\.xml$/,
-                use: ['xml-loader']
-              },
-              {
-                test: /\.csv$/,
-                use: ['csv-loader']
-              }
-        
+  return loaders
+}
 
-        ]
-    }
+module.exports = {
+  context: path.resolve(__dirname,"src"),
+  mode: "development", // operating mode webpack
+  entry: "./index.js", // entry file
+  output: {
+    path: path.resolve(__dirname, "dist"),  // path to final build folder
+    filename: "[name].[hash].js" // how to name files
+  },
+  plugins: [
+    new HTMLWebpackPlugin({ 
+      template: "./index.html",
+      favicon:"favicon.ico"
+    }), // create index.html and add in him links to scripts
+    new CleanWebpackPlugin() // cleaning from previous builds
+  ],
+  module: {
+    rules: [ // types of processed files and their loaders
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.less$/,
+        use: cssLoaders('less-loader')
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: cssLoaders('sass-loader')
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.xml$/,
+        use: ['xml-loader']
+      },
+      {
+        test: /\.csv$/,
+        use: ['csv-loader']
+      }
+
+
+    ]
+  }
 }
